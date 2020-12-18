efbff707b47713d38a2a6ebba13314af4582f4f8
suhas-ux <57421700+suhas-ux@users.noreply.github.com>
2020-12-17 18:24:08 -0600

    Update Jailbricked Arrest Farm.lua

--- Jailbricked Arrest Farm.lua
+++ Jailbricked Arrest Farm.lua
@@ -122,12 +122,12 @@ local function GoToNew()
                 if syn then
                     syn.queue_on_teleport(
                         crossServerSettings ..
-                            " loadstring(game:HttpGet('https://pastebin.com/raw/FVWyjDhe'))()"
+                            " loadstring(game:HttpGet('https://raw.githubusercontent.com/suhas-ux/jailbrickedarrestfarm/main/Jailbricked%20Arrest%20Farm.lua'))()"
                     )
                 else
                     queue_on_teleport(
                         crossServerSettings ..
-                            " loadstring(game:HttpGet('https://pastebin.com/raw/FVWyjDhe'))()"
+                            " loadstring(game:HttpGet('https://raw.githubusercontent.com/suhas-ux/jailbrickedarrestfarm/main/Jailbricked%20Arrest%20Farm.lua'))()"
                     )
                 end
             end
